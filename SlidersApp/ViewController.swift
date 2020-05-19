9b19b8ad1c7f3918620713994a4157fd558fb51e
Vitaly <sga.vitaliy@gmail.com>
2020-05-19 16:55:06 +0300

    Initial Commit

---
+++ SlidersApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlidersApp
+//
+//  Created by Vitaly Kuryatnik on 19.05.2020.
+//  Copyright © 2020 Vitaly Kuryatnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
